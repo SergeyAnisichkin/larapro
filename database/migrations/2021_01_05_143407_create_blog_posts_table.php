b3cfc67138ba6942b54fe8b60053a12e2ee0fcf6
Sergey Anisichikin <s.anisichkin@gmail.com>
2021-01-06 15:47:16 +0400

    change blog migration

--- database/migrations/2021_01_05_143407_create_blog_posts_table.php
+++ database/migrations/2021_01_05_143407_create_blog_posts_table.php
@@ -14,8 +14,28 @@ class CreateBlogPostsTable extends Migration
     public function up()
     {
         Schema::create('blog_posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+
+            $table->unsignedInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('slug')->unique();
+            $table->string('title');
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw');
+            $table->text('content_html');
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('blog_categories');
+            $table->index('is_published');
         });
     }
 
