0395450ab4843d3ff8731e922aab80eaf3643c25
Sergey Anisichikin <s.anisichkin@gmail.com>
2021-01-06 23:07:12 +0400

    add Controllers Blog posts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+Route::resource('rest', 'App\Http\Controllers\RestTestController')->names('restTest');
