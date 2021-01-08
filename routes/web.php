665329e57ae439d56799b2b2955d89e0c631ed81
Sergey Anisichikin <s.anisichkin@gmail.com>
2021-01-08 18:42:33 +0300

    add admin category

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-Route::resource('rest', 'App\Http\Controllers\RestTestController')->names('restTest');
+//>  Blog Admin
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store', ];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+//<
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::resource('rest', 'App\Http\Controllers\RestTestController')->names('restTest');
+
+
