04f07db3185f4df9fa9efc49b9563cc3313aa336
Sergey <s.anisichkin@gmail.com>
2021-05-13 22:57:55 +0300

    add Title MetaTag layouts/app_admin

--- app/Http/Controllers/Shop/Admin/MainController.php
+++ app/Http/Controllers/Shop/Admin/MainController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Shop\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MetaTag;
 
 class MainController extends AdminBaseController
 {
     public function index()
     {
+        MetaTag::setTags(['title' => 'Админ панель']);
+
         return view('shop.admin.main.index');
     }
 }
