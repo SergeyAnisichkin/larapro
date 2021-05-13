3933c819fec55f9e96a23e035411c87dcf28c1ed
Sergey <s.anisichkin@gmail.com>
2021-05-13 21:58:45 +0300

    composer update and add components for main admin index

--- resources/views/shop/admin/main/index.blade.php
+++ resources/views/shop/admin/main/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app_admin')
 
 @section('content')
-    ADMIN HELLO
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        @component('shop.admin.components.breadcrumb')
+            @slot('title') Панель упраления @endslot
+            @slot('parent') Главная @endslot
+            @slot('active')  @endslot
+        @endcomponent
+    </section>
 @endsection
