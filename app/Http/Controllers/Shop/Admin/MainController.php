b9145534790f999aec13ab3981cf3fcf0c1c657f
Sergey <s.anisichkin@gmail.com>
2021-05-15 20:29:05 +0300

    add last orders and products to Main Controller

--- app/Http/Controllers/Shop/Admin/MainController.php
+++ app/Http/Controllers/Shop/Admin/MainController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\Shop\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Admin\OrderRepository;
+use App\Repositories\Admin\ProductRepository;
 use Illuminate\Http\Request;
 use App\Repositories\Admin\MainRepository;
 use MetaTag;
 
 class MainController extends AdminBaseController
 {
+    private $orderRepository;
+    private $productRepository;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->orderRepository = app(OrderRepository::class);
+        $this->productRepository = app(ProductRepository::class);
+    }
+
+
     public function index()
     {
         MetaTag::setTags(['title' => 'Админ панель']);
@@ -18,7 +32,12 @@ class MainController extends AdminBaseController
         $countProducts = MainRepository::getCountProducts();
         $countCategories = MainRepository::getCountCategories();
 
+        $perPage = 4;
+        $last_orders = $this->orderRepository->getAllOrders($perPage);
+        $last_products = $this->productRepository->getLastProducts($perPage);
+
         return view('shop.admin.main.index',
-            compact('countOrders', 'countUsers', 'countProducts', 'countCategories'));
+            compact('countOrders', 'countUsers', 'countProducts', 'countCategories',
+                'last_orders', 'last_products'));
     }
 }
