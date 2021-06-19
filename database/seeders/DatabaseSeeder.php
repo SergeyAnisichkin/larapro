4e29f5605263d93a1f948f6ff8818af8fdb792d8
test server <s.anisichkin@gmail.com>
2021-06-19 08:33:41 +0000

    del migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,7 @@ class DatabaseSeeder extends Seeder
     {
        // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
-        $this->call(BlogCategoriesTableSeeder::class);
-        \App\Models\BlogPost::factory(100)->create();
         $this->call(RolesTableSeeder::class);
         $this->call(UserRolesTableSeeder::class);
-        $this->call(AttributeGroupsSeeder::class);
-        $this->call(AttributeProductsSeeder::class);
-        $this->call(AttributeValuesSeeder::class);
-        $this->call(BrandsSeeder::class);
-        $this->call(CategoriesSeeder::class);
-        $this->call(CurrenciesSeeder::class);
-        $this->call(GalleriesSeeder::class);
-        $this->call(ProductsSeeder::class);
-        $this->call(RelatedProductsSeeder::class);
-        $this->call(OrdersSeeder::class);
-        $this->call(AdminOrderProductsSeeder::class);
     }
 }
