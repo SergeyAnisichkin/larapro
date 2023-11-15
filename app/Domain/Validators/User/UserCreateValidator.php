bc4f925b4cbd8c4cb3afa0b6d0e453be0397fb6d
Sergey <s.anisichkin@gmail.com>
2023-11-15 17:14:44 +0300

    add Domain, UserCreateValidator

---
+++ app/Domain/Validators/User/UserCreateValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Validators\User;
+
+
+//use Carbon\CarbonImmutable;
+//use Illuminate\Support\Carbon;
+
+class UserCreateValidator
+{
+//    private array $messages = [];
+//
+//    public function __construct(private readonly ElnDataAttributeService $elnDataService)
+//    {
+//    }
+//
+//    public function getErrorMessage(): string
+//    {
+//        $result = implode($this->messages);
+//        $this->messages = [];
+//
+//        return $result;
+//    }
+//
+//    public function validateForCalculation(Eln $eln): bool
+//    {
+//    }
+}
